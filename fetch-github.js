31b42c48d4ccdbd27483615f64e5a5afd379e37c
Finini Nutrition <l.condefigueroa2512@student.sbccd.edu>
2026-02-21 18:15:13 -0800

    Update fetch-github.js

--- fetch-github.js
+++ fetch-github.js
@@ -1,6 +1,6 @@
 document.getElementById("generateReceiptBtn").addEventListener("click", function () {
     // Trigger the API call to generate the PDF when the button is clicked
-    fetch('http://<YOUR-TERMUX-IP>:3000/generate-pdf')  // Change <YOUR-TERMUX-IP> to the Termux IP address
+    fetch('https://5c1923faa907cb9a-172-56-181-24.serveousercontent.com')  // Change <YOUR-TERMUX-IP> to the Termux IP address
         .then(response => response.blob())  // Get the PDF as a blob
         .then(pdfBlob => {
             const link = document.createElement('a');
